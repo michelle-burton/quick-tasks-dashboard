3a7b91d8e59e0de910a5b05173ad0045d6a601d2
Michelle Burton <msmichelleburton@yahoo.com>
2025-04-03 08:26:00 -0500

    added page styles

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("task-form");
+const input = document.getElementById("task-input");
+const list = document.getElementbyId("task-list");
+const summary = document.getElementbyId("summary");
+
+let tasks = [];
+
+// ADD A TASK
+
+// Form Submission 
+// event listner, 
+// prevent default form behavior )page reload)
+
+// get the input text, .trim()
+
+// validate input 
+
+//create a new task obj: id, text, status(completed
+
+// add task to our array of tasks - push the obje
+
+// clear the input field - reset ''
+
+// re-render the task-list - call renderTasks() to update ui
